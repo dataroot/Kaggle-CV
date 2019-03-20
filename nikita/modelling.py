5e354b8bd70331e9acd7a9c99cc0ce172eca8b79
nikitosoleil <nikiolei@gmail.com>
2019-03-20 14:11:34 +0200

    First model run

--- nikita/modelling.py
+++ nikita/modelling.py
@@ -5,17 +5,19 @@ from keras.layers import Input, Dense, Lambda, Embedding, Concatenate
 class Encoder(Model):
     def __init__(self, input_dim: int, output_dim: int, emb_input_dims: list, emb_output_dims: list):
         inputs = Input((input_dim, ))
-        
         n_embs = len(emb_input_dims)
-        embs = []
-        for i, nunique, dim in zip(range(n_embs), emb_input_dims, emb_output_dims):
-            tmp = Lambda(lambda x: x[:, i])(inputs)
-            embs.append(Embedding(nunique, dim)(tmp))
-        embs.append(Lambda(lambda x: x[:, n_embs:])(inputs))
         
-        self.inter = Concatenate()(embs)
+        if n_embs > 0:
+            embs = []
+            for i, nunique, dim in zip(range(n_embs), emb_input_dims, emb_output_dims):
+                tmp = Lambda(lambda x: x[:, i])(inputs)
+                embs.append(Embedding(nunique, dim)(tmp))
+            embs.append(Lambda(lambda x: x[:, n_embs:])(inputs))
+            self.inter = Concatenate()(embs)
+        else:
+            self.inter = inputs
+           
         outputs = Dense(output_dim)(self.inter)
-        
         super().__init__(inputs, outputs)
         
 class Merger(Model):
@@ -36,7 +38,14 @@ class Mothership(Model):
         
         self.que_model = Encoder(que_dim, inter_dim, que_input_embs, que_output_embs)
         self.pro_model = Encoder(pro_dim, inter_dim, pro_input_embs, pro_output_embs)
-        self.merge_model = Merger(inter_dim)
         
+        self.merged = Lambda(lambda x: tf.reduce_sum(tf.square(x[0]-x[1]), axis = -1)) \
+            ([self.que_model.outputs[0], self.pro_model.outputs[0]])
+        outputs = Lambda(lambda x: tf.reshape(tf.exp(-self.merged), (-1, 1)))(self.merged)
+        super().__init__([self.que_model.inputs[0], self.pro_model.inputs[0]], outputs)
+        
+        '''
+        self.merge_model = Merger(inter_dim)
         super().__init__([self.que_model.inputs[0], self.pro_model.inputs[0]], 
                          self.merge_model([self.pro_model.outputs[0], self.que_model.outputs[0]]))
+        '''
