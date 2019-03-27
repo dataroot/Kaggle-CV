ebe0afc79b98c8e71554b71200679abad1cb8455
Kostyan0005 <rkd000521@gmail.com>
2019-03-27 19:03:05 +0200

    Change batch generator

--- kostya/batch_generator.py
+++ kostya/batch_generator.py
@@ -2,8 +2,8 @@ import pickle
 import random
 
 import keras
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from utils import TextProcessor
 
@@ -28,7 +28,7 @@ class BatchGenerator(keras.utils.Sequence):
         
         que_tags = que.merge(tag_que, left_on = 'questions_id', right_on = 'tag_questions_question_id').merge(tags, left_on = 'tag_questions_tag_id', right_on = 'tags_tag_id')
         que_tags = que_tags[['questions_id', 'tags_tag_name']].groupby(by = 'questions_id', as_index = False).aggregate(lambda x: ' '.join(x))
-        self.que_tag = {row['questions_id']: row['tags_tag_name'].split() for i, row in que_tags.iterrows()}
+        self.que_tag = {row['questions_id']: row['tags_tag_name'].split() for _, row in que_tags.iterrows()}
         
         ans_que = ans.merge(que, left_on = 'answers_question_id', right_on = 'questions_id')
         ans_que_pro = ans_que.merge(pro, left_on = 'answers_author_id', right_on = 'professionals_id')
@@ -37,13 +37,21 @@ class BatchGenerator(keras.utils.Sequence):
         self.ques = list(set(ans_que_pro['questions_id']))
         self.pros = list(set(ans_que_pro['professionals_id']))
         
-        self.que_pro_set = {(row['questions_id'], row['professionals_id']) for i, row in ans_que_pro.iterrows()}
+        self.que_pro_set = {(row['questions_id'], row['professionals_id']) for _, row in ans_que_pro.iterrows()}
         self.que_pro_list = list(self.que_pro_set)
         
         with open('tags_embs.pickle', 'rb') as file:
             self.tag_emb = pickle.load(file)
         with open('industries_embs.pickle', 'rb') as file:
             self.ind_emb = pickle.load(file)
+        
+        #------------------------------------------------------------------
+        #                    THE CODE I ADDED
+        #------------------------------------------------------------------
+        
+        # Add answer list and a dictionary mapping answer to (question, professional) pair
+        self.ans_que_pro_dict = {row['answers_id']:(row['questions_id'], row['professionals_id'])
+                                 for _, row in ans_que_pro.iterrows()}
         
         # Load que and pro statistical features
         with open('que_feature_dict.pickle', 'rb') as f:
@@ -56,8 +64,13 @@ class BatchGenerator(keras.utils.Sequence):
             self.pro_last_answer_dates_dict = pickle.load(f)
         with open('ans_date_added_dict.pickle', 'rb') as f:
             self.ans_date_added_dict = pickle.load(f)
-        with open('que_last_answer_date_dict.pickle', 'rb') as f:
-            self.que_last_answer_date_dict = pickle.load(f)
+        with open('ans_last_answer_date_dict.pickle', 'rb') as f:
+            self.ans_last_answer_date_dict = pickle.load(f)
+        with open('ans_sampled_profs_dict.pickle', 'rb') as f:
+            self.ans_sampled_profs_dict = pickle.load(f)
+        with open('ans_list.pickle', 'rb') as f:
+            self.ans_list = pickle.load(f)
+        #------------------------------------------------------------------
     
     
     def __len__(self):
@@ -79,19 +92,18 @@ class BatchGenerator(keras.utils.Sequence):
         return np.vstack(x_que), np.vstack(x_pro)
     
     
-    def __negative_que_last_answer_date(self, que, pro) -> (np.float64, bool):
-        ans_date = self.ans_date_added_dict[que]
+    def __negative_que_last_answer_date(self, ans, pro):
+        ans_date = self.ans_date_added_dict[ans]
         pro_dates = self.pro_last_answer_dates_dict[pro]
         
         index = np.searchsorted(pro_dates, ans_date)
         if index == 0:
-            return ans_date, False
-        
-        return pro_dates[index-1], True     
+            raise ValueError("Index cannot be zero.")
+        return pro_dates[index-1]     
     
     
     def __getitem__(self, index):
-        pos_pairs = self.que_pro_list[self.pos_size * index: self.pos_size * (index + 1)]
+        pos_pairs = []
         neg_pairs = []
         
         pos_last_dates = []
@@ -100,25 +112,25 @@ class BatchGenerator(keras.utils.Sequence):
         pos_que_features, pos_pro_features = [], []
         neg_que_features, neg_pro_features = [], []
         
-        for que, pro in pos_pairs:
-            pos_last_dates.append(self.que_last_answer_date_dict[que])
+        pos_ans = self.ans_list[self.pos_size * index: self.pos_size * (index + 1)]
+        for ans in pos_ans:
+            que, pro = self.ans_que_pro_dict[ans]
+            pos_pairs.append((que, pro))
             pos_que_features.append(self.que_feature_dict[que])
             pos_pro_features.append(self.pro_feature_dict[pro])
+            pos_last_dates.append(self.ans_last_answer_date_dict[ans])
         
         for i in range(self.neg_size):
-            while True:
-                que = random.choice(self.ques)
-                pro = random.choice(self.pros)
-                
-                # Find last answer date 
-                last_date, valid_time = self.__negative_que_last_answer_date(que, pro)
-                
-                if (que, pro) not in self.que_pro_set and valid_time:
-                    neg_pairs.append((que, pro))
-                    neg_last_dates.append(last_date)
-                    neg_que_features.append(self.que_feature_dict[que])
-                    neg_pro_features.append(self.pro_feature_dict[pro])
-                    break
+            ans = random.choice(self.ans_list)
+            que, _ = self.ans_que_pro_dict[ans]
+            pro = random.choice(self.ans_sampled_profs_dict[ans])
+            
+            # Add que and pro data to all required lists
+            last_date = self.__negative_que_last_answer_date(ans, pro)
+            neg_pairs.append((que, pro))
+            neg_que_features.append(self.que_feature_dict[que])
+            neg_pro_features.append(self.pro_feature_dict[pro])
+            neg_last_dates.append(last_date)
         
         pos_que_embeddings, pos_pro_embeddings = self.__convert(pos_pairs)
         neg_que_embeddings, neg_pro_embeddings = self.__convert(neg_pairs)
@@ -135,5 +147,5 @@ class BatchGenerator(keras.utils.Sequence):
     
     
     def on_epoch_end(self):
-        self.que_pro_list = random.sample(self.que_pro_list, len(self.que_pro_list))
+        np.random.shuffle(self.ans_list)
 
