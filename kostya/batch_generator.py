206bb09cdcc3a9a38a7143d774cec30f03be61a8
Kostyan0005 <rkd000521@gmail.com>
2019-03-31 17:29:24 +0300

    Finish distribution computation with current time and try new batch generator

--- kostya/batch_generator.py
+++ kostya/batch_generator.py
@@ -34,15 +34,12 @@ class BatchGenerator(keras.utils.Sequence):
         ans_que_pro = ans_que.merge(pro, left_on = 'answers_author_id', right_on = 'professionals_id')
         ans_que_pro = ans_que_pro.merge(stu, left_on = 'questions_author_id', right_on = 'students_id')
         
-        self.ques = list(set(ans_que_pro['questions_id']))
-        self.pros = list(set(ans_que_pro['professionals_id']))
-        
         self.que_pro_set = {(row['questions_id'], row['professionals_id']) for _, row in ans_que_pro.iterrows()}
         self.que_pro_list = list(self.que_pro_set)
         
-        with open('tags_embs.pickle', 'rb') as file:
+        with open('tags_embs.pkl', 'rb') as file:
             self.tag_emb = pickle.load(file)
-        with open('industries_embs.pickle', 'rb') as file:
+        with open('industries_embs.pkl', 'rb') as file:
             self.ind_emb = pickle.load(file)
         
         #------------------------------------------------------------------
@@ -59,17 +56,25 @@ class BatchGenerator(keras.utils.Sequence):
         with open('pro_feature_dict.pickle', 'rb') as f:
             self.pro_feature_dict = pickle.load(f)
         
-        # Load pro last answer dates dict and que answer date dict
-        with open('pro_last_answer_dates_dict.pickle', 'rb') as f:
-            self.pro_last_answer_dates_dict = pickle.load(f)
+        # Load time related dicts and lists
+        with open('pro_answer_dates_dict.pickle', 'rb') as f:
+            self.pro_answer_dates_dict = pickle.load(f)
         with open('ans_date_added_dict.pickle', 'rb') as f:
             self.ans_date_added_dict = pickle.load(f)
-        with open('ans_last_answer_date_dict.pickle', 'rb') as f:
-            self.ans_last_answer_date_dict = pickle.load(f)
-        with open('ans_sampled_profs_dict.pickle', 'rb') as f:
-            self.ans_sampled_profs_dict = pickle.load(f)
+        with open('que_date_added_dict.pickle', 'rb') as f:
+            self.que_date_added_dict = pickle.load(f)
+        with open('ans_prev_answer_date_dict.pickle', 'rb') as f:
+            self.ans_prev_answer_date_dict = pickle.load(f)
         with open('ans_list.pickle', 'rb') as f:
             self.ans_list = pickle.load(f)
+        with open('pro_list.pickle', 'rb') as f:
+            self.pro_list = pickle.load(f)
+        with open('pro_reg_date_list.pickle', 'rb') as f:
+            self.pro_reg_date_list = pickle.load(f)
+        
+        # Load preprocessors
+        with open('preprocessors.pickle', 'rb') as f:
+            self.preproc = pickle.load(f)
         #------------------------------------------------------------------
     
     
@@ -92,11 +97,10 @@ class BatchGenerator(keras.utils.Sequence):
         return np.vstack(x_que), np.vstack(x_pro)
     
     
-    def __negative_que_last_answer_date(self, ans, pro):
-        ans_date = self.ans_date_added_dict[ans]
-        pro_dates = self.pro_last_answer_dates_dict[pro]
+    def __negative_que_prev_answer_date(self, pro, cur_time):
+        pro_dates = self.pro_answer_dates_dict[pro]
         
-        index = np.searchsorted(pro_dates, ans_date)
+        index = np.searchsorted(pro_dates, cur_time)
         if index == 0:
             raise ValueError("Index cannot be zero.")
         return pro_dates[index-1]     
@@ -106,8 +110,11 @@ class BatchGenerator(keras.utils.Sequence):
         pos_pairs = []
         neg_pairs = []
         
-        pos_last_dates = []
-        neg_last_dates = []
+        pos_prev_dates = []
+        neg_prev_dates = []
+        
+        pos_cur_times = []
+        neg_cur_times = []
         
         pos_que_features, pos_pro_features = [], []
         neg_que_features, neg_pro_features = [], []
@@ -118,29 +125,92 @@ class BatchGenerator(keras.utils.Sequence):
             pos_pairs.append((que, pro))
             pos_que_features.append(self.que_feature_dict[que])
             pos_pro_features.append(self.pro_feature_dict[pro])
-            pos_last_dates.append(self.ans_last_answer_date_dict[ans])
+            pos_prev_dates.append(self.ans_prev_answer_date_dict[ans])
+            pos_cur_times.append(self.ans_date_added_dict[ans])
         
         for i in range(self.neg_size):
             ans = random.choice(self.ans_list)
             que, _ = self.ans_que_pro_dict[ans]
-            pro = random.choice(self.ans_sampled_profs_dict[ans])
+            
+            
+            #------------------------------------------------------------------
+            #                    DISTRIBUTION COMPUTATION
+            #------------------------------------------------------------------
+            mean = 3
+            sample = np.random.exponential(mean)
+            while sample >= 6:
+                sample = np.random.exponential(mean)
+            cur_time = np.exp(sample) - 1
+            
+            cur_time = (self.preproc['questions_date_added_time']
+                        .inverse_transform([[self.que_date_added_dict[que]]])[0][0] + cur_time / 365)
+            
+            threshold = np.searchsorted(self.pro_reg_date_list, cur_time)
+            valid_pros = self.pro_list[:threshold]
+            
+            # Sample 50 pros among valid ones if the number of valid ones is greater than 50
+            if len(valid_pros) > 50:
+                sampled_pros = np.random.choice(valid_pros, 50, replace=False)
+            else:
+                sampled_pros = valid_pros
+            
+            cur_time = self.preproc['professionals_prev_answer_date_time'].transform([[cur_time]])[0][0]
+            
+            pros = []
+            prev_answer_dates = []
+            
+            for pro in sampled_pros:
+                if (que, pro) not in self.que_pro_set:
+                    prev_answer_date = self.__negative_que_prev_answer_date(pro, cur_time)
+                    
+                    pros.append(pro)
+                    prev_answer_dates.append(prev_answer_date)
+            
+            if len(pros) == 0:
+                continue
+            
+            # Substact prev answer dates from cur_time
+            distances = cur_time - np.array(prev_answer_dates)
+            
+            # Apply log1p transformation to 1 / distances and normalize each entry
+            distances = np.log1p(1 / distances)
+            distances /= distances.sum()
+            
+            # Sample one professional from distribution of distances
+            pro = np.random.choice(pros, p=distances)
+            #-------------------------------------------------------------------
+            
             
             # Add que and pro data to all required lists
-            last_date = self.__negative_que_last_answer_date(ans, pro)
+            prev_date = self.__negative_que_prev_answer_date(pro, cur_time)
             neg_pairs.append((que, pro))
             neg_que_features.append(self.que_feature_dict[que])
             neg_pro_features.append(self.pro_feature_dict[pro])
-            neg_last_dates.append(last_date)
+            neg_prev_dates.append(prev_date)
+            neg_cur_times.append(cur_time)
         
         pos_que_embeddings, pos_pro_embeddings = self.__convert(pos_pairs)
         neg_que_embeddings, neg_pro_embeddings = self.__convert(neg_pairs)
         
-        x_pos_que = np.hstack([np.array(pos_que_features), pos_que_embeddings])
-        x_neg_que = np.hstack([np.array(neg_que_features), neg_que_embeddings])
+        x_pos_que = np.hstack(
+            [np.array(pos_que_features),
+#              np.array(pos_cur_times)[:, np.newaxis],
+             pos_que_embeddings])
+        x_neg_que = np.hstack(
+            [np.array(neg_que_features),
+#              np.array(neg_cur_times)[:, np.newaxis],
+             neg_que_embeddings])
         
-        # print(np.array(pos_pro_features).shape, np.array(pos_last_dates)[:, np.newaxis].shape, pos_pro_embeddings.shape)
-        x_pos_pro = np.hstack([np.array(pos_pro_features), np.array(pos_last_dates)[:, np.newaxis], pos_pro_embeddings])
-        x_neg_pro = np.hstack([np.array(neg_pro_features), np.array(neg_last_dates)[:, np.newaxis], neg_pro_embeddings])
+        x_pos_pro = np.hstack(
+            [np.array(pos_pro_features),
+#              np.array(pos_prev_dates)[:, np.newaxis],
+#              np.array(pos_cur_times)[:, np.newaxis],
+             pos_pro_embeddings])
+        x_neg_pro = np.hstack(
+            [np.array(neg_pro_features),
+#              np.array(neg_prev_dates)[:, np.newaxis],
+#              np.array(neg_cur_times)[:, np.newaxis],
+             neg_pro_embeddings])
         
         return [np.vstack([x_pos_que, x_neg_que]), np.vstack([x_pos_pro, x_neg_pro])], \
                 np.vstack([np.ones((self.pos_size, 1)), np.zeros((self.neg_size, 1))])
