5c873e710b0354921c6af5086a15c52455a4ba51
nikitosoleil <nikiolei@gmail.com>
2019-04-06 00:57:51 +0300

    First updated pipeline run

--- proj/generator.py
+++ proj/generator.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 
 
+# TODO: add current time feature
+
 class BatchGenerator(keras.utils.Sequence):
     """
     Class to ingest data from pre-processed DataFrames to model
@@ -13,6 +15,7 @@ class BatchGenerator(keras.utils.Sequence):
 
     def __init__(self, que: pd.DataFrame, stu: pd.DataFrame, pro: pd.DataFrame,
                  batch_size: int, pos_pairs: list, nonneg_pairs: list):
+        print(que.dtypes, stu.dtypes, pro.dtypes)
         """
         :param que: pre-processed questions data
         :param pro: pre-processed professionals data
@@ -31,15 +34,15 @@ class BatchGenerator(keras.utils.Sequence):
         self.que_feat = {que_ar[i, 0]: que_ar[i, 2:] for i in range(que_ar.shape[0])}
         self.que_time = {que_ar[i, 0]: que_ar[i, 1] for i in range(que_ar.shape[0])}
 
-        self.pro_feat = {}
-        for pro_id, group in pro.groupby('professionals_id').groups():
-            group_ar = group.values()
-            self.pro_feat[pro_id] = [(group_ar[i, 0], group_ar[i, 1:]) for i in range(group_ar.shape[0])]
-
         self.stu_feat = {}
-        for stu_id, group in stu.groupby('students_id').groups():
-            group_ar = group.values()
+        for stu_id, group in stu.groupby('students_id'):
+            group_ar = group.values[:, 1:]
             self.stu_feat[stu_id] = [(group_ar[i, 0], group_ar[i, 1:]) for i in range(group_ar.shape[0])]
+
+        self.pro_feat = {}
+        for pro_id, group in pro.groupby('professionals_id'):
+            group_ar = group.values[:, 1:]
+            self.pro_feat[pro_id] = [(group_ar[i, 0], group_ar[i, 1:]) for i in range(group_ar.shape[0])]
 
     def __len__(self):
         return len(self.pos_pairs) // self.batch_size
@@ -50,9 +53,21 @@ class BatchGenerator(keras.utils.Sequence):
         of question and professionals features
         """
         x_que, x_pro = [], []
-        for i, (que, pro) in enumerate(pairs):
-            x_que.append(self.que_feat[que])
-            x_pro.append(self.pro_feat[pro])
+        for (que, stu, pro), time in zip(pairs, times):
+            que_data = self.que_feat[que]
+
+            def find(ar: list, t: str):
+                ret = ar[0][1]
+                for i, (time_i, data_i) in enumerate(ar):
+                    if not time_i or time_i < t:
+                        ret = data_i
+                return ret
+
+            stu_data = find(self.stu_feat[stu], time)
+            pro_data = find(self.pro_feat[pro], time)
+
+            x_que.append(np.hstack([stu_data, que_data]))
+            x_pro.append(pro_data)
         return np.vstack(x_que), np.vstack(x_pro)
 
     def __getitem__(self, index):
@@ -77,7 +92,7 @@ class BatchGenerator(keras.utils.Sequence):
                         shift = np.random.exponential(50) - 35
                         if shift > 0:
                             break
-                    neg_times.append(zero + np.timedelta64(shift, 'D'))
+                    neg_times.append(np.datetime64(zero) + np.timedelta64(int(shift * 24 * 60), 'm'))
                     break
 
         x_pos_que, x_pos_pro = self.__convert(pos_pairs, pos_times)
--- proj/main.py
+++ proj/main.py
@@ -32,18 +32,20 @@ def drive(data_path: str, dump_path: str, split_date: str):
 
     # pipeline_d2v(train['que'], train['ans'], train['pro'], tags, 10)
 
-    model = Mothership(que_dim=25, que_input_embs=[102, 42], que_output_embs=[2, 2],
-                       pro_dim=21, pro_input_embs=[102, 102, 42], pro_output_embs=[2, 2, 2], inter_dim=10)
+    model = Mothership(que_dim=30, que_input_embs=[102, 42], que_output_embs=[2, 2],
+                       pro_dim=25, pro_input_embs=[102, 102, 42], pro_output_embs=[2, 2, 2], inter_dim=10)
 
     nonneg_pairs = []
 
     for mode, data in [('Train', train), ('Test', test)]:
         print(mode)
 
-        df = data['que'].merge(data['ans'], left_on='questions_id', right_on='answers_question_id')
+        df = data['que'].merge(data['ans'], left_on='questions_id', right_on='answers_question_id') \
+            .merge(data['pro'], left_on='answers_author_id', right_on='professionals_id') \
+            .merge(data['stu'], left_on='questions_author_id', right_on='students_id')
         if mode == 'Test':
             df = df.loc[df['answers_date_added'] >= split_date]
-        df = df[['questions_id', 'questions_author_id', 'answers_author_id']]
+        df = df[['questions_id', 'students_id', 'professionals_id']]
         pos_pairs = list(df.itertuples(index=False))
         nonneg_pairs += pos_pairs
 
@@ -63,20 +65,18 @@ def drive(data_path: str, dump_path: str, split_date: str):
 
         print(que_data.columns, stu_data.columns, pro_data.columns)
 
-        raise NotImplementedError()
-
         bg = BatchGenerator(que_data, stu_data, pro_data, 64, pos_pairs, nonneg_pairs)
 
         if mode == 'Train':
             model.compile(Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
-            model.fit_generator(bg, epochs=10, verbose=2)
+            model.fit_generator(bg, epochs=3, verbose=2)
             model.save_weights(dump_path + 'model.h5')
         else:
             print(model.evaluate_generator(bg))
 
         bg = BatchGenerator(que_data, stu_data, pro_data, 2048, pos_pairs, nonneg_pairs)
 
-        fn = {"que": list(que_data.columns[2:]), "pro": list(pro_data.columns[2:]),
+        fn = {"que": list(stu_data.columns[2:]) + list(que_data.columns[2:]), "pro": list(pro_data.columns[2:]),
               'text': [f'que_emb_{i}' for i in range(10)] + [f'pro_emb_{i}' for i in range(10)]}
 
         fi = permutation_importance(model, bg[0][0][0], bg[0][0][1], bg[0][1], fn, n_trials=5)
