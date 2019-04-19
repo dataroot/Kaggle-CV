e747aaa3cf025d84d2afb0b35c55e83d3dd7e396
nikitosoleil <nikiolei@gmail.com>
2019-04-19 14:06:48 +0300

    Predictor finished

--- proj/predictor.py
+++ proj/predictor.py
@@ -2,6 +2,7 @@ import pickle
 
 import pandas as pd
 import numpy as np
+import keras
 
 from sklearn.neighbors import KDTree
 
@@ -10,37 +11,39 @@ from processors import QueProc, ProProc
 
 class Predictor:
     """
-    Class that creates KNN tree for professionals
-    and which is used to find closest professionals for a particular question
+    Class for handling closest professionals or questions queries
     """
 
-    def __init__(self, model, path):
+    def __init__(self, model: keras.Model, path: str):
         """
-        Prepare required datasets and create KNN tree for professionals
-        based on latent vectors from content model
         :param model: compiled Keras model
+        :param path: path to dump data
         """
         self.model = model
 
-        # form question-student pairs dataframe
+        # load question-student pairs
         with open(path + 'que_stu_pairs.pkl', 'rb') as file:
             pairs = pickle.load(file)
 
-        # load datasets with preprocessed features
+        # load preprocessed data
         store = pd.HDFStore(path + 'processed.h5', 'r')
         que_data = pd.read_hdf(store, 'que')
         stu_data = pd.read_hdf(store, 'stu')
         pro_data = pd.read_hdf(store, 'pro')
 
+        # construct mappings from entity id to features
         self.que_dict = {row.values[0]: row.values[2:] for i, row in que_data.iterrows()}
         self.stu_dict = {stu: group.values[-1, 2:] for stu, group in stu_data.groupby('students_id')}
         self.pro_dict = {pro: group.values[-1, 2:] for pro, group in pro_data.groupby('professionals_id')}
+
+        # form final features for all known questions and professionals
 
         que_feat, que_ids, pro_feat, pro_ids = [], [], [], []
 
         for que in self.que_dict.keys():
             cur_stu = pairs[que]
             if cur_stu in self.stu_dict:
+                # actual question's features are both question and student's features
                 que_feat.append(np.hstack([self.stu_dict[cur_stu], self.que_dict[que]]))
                 que_ids.append(que)
 
@@ -59,43 +62,51 @@ class Predictor:
 
         # compute latent vectors for questions and professionals
         self.que_lat_vecs = self.que_model.predict(self.que_feat)
-
-        print(self.pro_feat[2637])
         self.pro_lat_vecs = self.pro_model.predict(self.pro_feat)
-        print(self.pro_lat_vecs[2637])
 
-        # create two KNN trees consisting of question and professional latent vectors
+        # create KDTree trees from question and professional latent vectors
         self.que_tree = KDTree(self.que_lat_vecs)
         self.pro_tree = KDTree(self.pro_lat_vecs)
 
-        # initialize QueProc and ProProc
+        # initialize preprocessors
         self.que_proc = QueProc(oblige_fit=False, path=path)
         self.pro_proc = ProProc(oblige_fit=False, path=path)
 
-    def __get_que_latent(self, que_df: pd.DataFrame, que_tags: pd.DataFrame):
+    def __get_que_latent(self, que_df: pd.DataFrame, que_tags: pd.DataFrame) -> np.ndarray:
+        """
+        Get latent vectors for questions in raw format
+        """
         que_df['questions_date_added'] = pd.to_datetime(que_df['questions_date_added'])
 
+        # extract and preprocess question's features
         que_feat = self.que_proc.transform(que_df, que_tags).values[:, 2:]
 
+        # actual question's features are both question and student's features
         stu_feat = np.vstack([self.stu_dict[stu] for stu in que_df['questions_author_id']])
         que_feat = np.hstack([stu_feat, que_feat])
 
+        # encode question's data to get latent representation
         lat_vecs = self.que_model.predict(que_feat)
 
         return lat_vecs
 
     def __get_pro_latent(self, pro_df: pd.DataFrame, que_df: pd.DataFrame, ans_df: pd.DataFrame,
-                         pro_tags: pd.DataFrame):
+                         pro_tags: pd.DataFrame) -> np.ndarray:
+        """
+        Get latent vectors for professionals in raw format
+        """
         pro_df['professionals_date_joined'] = pd.to_datetime(pro_df['professionals_date_joined'])
         que_df['questions_date_added'] = pd.to_datetime(que_df['questions_date_added'])
-        ans_df['answers_date_added'] = pd.to_datetime(que_df['questions_date_added'])
+        ans_df['answers_date_added'] = pd.to_datetime(ans_df['answers_date_added'])
 
+        # extract and preprocess professional's features
         pro_feat = self.pro_proc.transform(pro_df, que_df, ans_df, pro_tags)
+
+        # select the last available version of professional's features
         pro_feat = pro_feat.groupby('professionals_id').last().values[:, 1:]
 
-        print(pro_feat)
+        # encode professional's data to get latent representation
         lat_vecs = self.pro_model.predict(pro_feat)
-        print(lat_vecs)
 
         return lat_vecs
 
@@ -104,43 +115,132 @@ class Predictor:
         tuples = []
         for i, id in enumerate(ids):
             for j, sim_que in enumerate(sims[i]):
-                tuples.append((id, sim_que, scores[i, j]))
+                tuples.append((id, sim_que[0], scores[i, j]))
         score_df = pd.DataFrame(tuples, columns=['id', 'match_id', 'match_score'])
         return score_df
 
-    def __get_ques_by_latent(self, ids, lat_vecs, top):
+    def __get_ques_by_latent(self, ids: np.ndarray, lat_vecs: np.ndarray, top: int) -> pd.DataFrame:
+        """
+        Get top questions with most similar latent representations to given vectors
+        """
         dists, ques = self.que_tree.query(lat_vecs, k=top)
         ques = self.que_ids[ques]
         scores = np.exp(-dists)
         return Predictor.__construct_df(ids, ques, scores)
 
-    def __get_pros_by_latent(self, ids, lat_vecs, top):
+    def __get_pros_by_latent(self, ids: np.ndarray, lat_vecs: np.ndarray, top: int) -> pd.DataFrame:
+        """
+        Get top professionals with most similar latent representations to given vectors
+        """
         dists, pros = self.pro_tree.query(lat_vecs, k=top)
         pros = self.pro_ids[pros]
         scores = np.exp(-dists)
         return Predictor.__construct_df(ids, pros, scores)
 
     def find_pros_by_que(self, que_df: pd.DataFrame, que_tags: pd.DataFrame, top: int = 10) -> pd.DataFrame:
+        """
+        Get top professionals with most similar internal representation to given questions
+
+        :param que_df: question's data in raw format
+        :param que_tags: questions's tags in raw format
+        :param top: number of professionals for each question to return
+        :return: dataframe of question's ids, matched professional's ids and similarity scores
+        """
         lat_vecs = self.__get_que_latent(que_df, que_tags)
         return self.__get_pros_by_latent(que_df['questions_id'].values, lat_vecs, top)
 
     def find_ques_by_que(self, que_df: pd.DataFrame, que_tags: pd.DataFrame, top: int = 10) -> pd.DataFrame:
+        """
+        Get top questions with most similar internal representation to given questions
+
+        :param que_df: question's data in raw format
+        :param que_tags: questions's tags in raw format
+        :param top: number of questions for each question to return
+        :return: dataframe of question's ids, matched question's ids and similarity scores
+        """
         lat_vecs = self.__get_que_latent(que_df, que_tags)
         return self.__get_ques_by_latent(que_df['questions_id'].values, lat_vecs, top)
 
-    # TODO: consider professionals which are already in base
-
     def find_ques_by_pro(self, pro_df: pd.DataFrame, que_df: pd.DataFrame, ans_df: pd.DataFrame,
                          pro_tags: pd.DataFrame, top: int = 10) -> pd.DataFrame:
+        """
+        Get top questions with most similar internal representation to given professional
+
+        :param pro_df: professional's data in raw format
+        :param que_df: question's data in raw format
+        :param ans_df: answer's data in raw format
+        :param pro_tags: professional's tags data in raw format
+        :param top: number of questions for each professional to return
+        :return: dataframe of professional's ids, matched question's ids and similarity scores
+        """
         lat_vecs = self.__get_pro_latent(pro_df, que_df, ans_df, pro_tags)
         return self.__get_ques_by_latent(pro_df['professionals_id'].values, lat_vecs, top)
 
     def find_pros_by_pro(self, pro_df: pd.DataFrame, que_df: pd.DataFrame, ans_df: pd.DataFrame,
                          pro_tags: pd.DataFrame, top: int = 10) -> pd.DataFrame:
+        """
+        Get top professionals with most similar internal representation to given professional
+
+        :param pro_df: professional's data in raw format
+        :param que_df: question's data in raw format
+        :param ans_df: answer's data in raw format
+        :param pro_tags: professional's tags data in raw format
+        :param top: number of questions for each professional to return
+        :return: dataframe of professional's ids, matched professional's ids and similarity scores
+        """
         lat_vecs = self.__get_pro_latent(pro_df, que_df, ans_df, pro_tags)
         return self.__get_pros_by_latent(pro_df['professionals_id'].values, lat_vecs, top)
 
-    # TODO: refactor this
+
+class Formatter:
+    """
+    Class with useful for Predictor input/output functionality
+    """
+
+    def __init__(self, data_path: str):
+        pro = pd.read_csv(data_path + 'professionals.csv')
+        que = pd.read_csv(data_path + 'questions.csv')
+
+        tags = pd.read_csv(data_path + 'tags.csv')
+        tag_users = pd.read_csv(data_path + 'tag_users.csv')
+        tag_que = pd.read_csv(data_path + 'tag_questions.csv')
+
+        tag_merged = tags.merge(tag_users, left_on='tags_tag_id', right_on='tag_users_tag_id')
+        tags_grouped = tag_merged.groupby('tag_users_user_id').agg(lambda x: ' '.join(x))[['tags_tag_name']]
+        self.pro = pro.merge(tags_grouped, left_on='professionals_id', right_index=True)
+
+        tag_merged = tags.merge(tag_que, left_on='tags_tag_id', right_on='tag_questions_tag_id')
+        tags_grouped = tag_merged.groupby('tag_questions_question_id').agg(lambda x: ' '.join(x))[['tags_tag_name']]
+        self.que = que.merge(tags_grouped, left_on='questions_id', right_index=True)
+
+    def get_que(self, scores: pd.DataFrame) -> pd.DataFrame:
+        """
+        Append all the question's data to question's scoring dataframe from Predictor
+
+        :param scores: result of similar questions query on Predictor object
+        :return: extended dataframe
+        """
+        return self.que.merge(scores, left_on='questions_id', right_on='match_id').sort_values('match_score',
+                                                                                               ascending=False)
+
+    def get_pro(self, scores: pd.DataFrame) -> pd.DataFrame:
+        """
+        Append all the professional's data to professional's scoring dataframe from Predictor
+
+        :param scores: result of similar professionals query on Predictor object
+        :return: extended dataframe
+        """
+        return self.pro.merge(scores, left_on='professionals_id', right_on='match_id').sort_values('match_score',
+                                                                                                   ascending=False)
+
+    @staticmethod
+    def __convert_tuples(ids, tags):
+        tuples = []
+        for i, tgs in enumerate(tags):
+            que = ids[i]
+            for tag in tgs.split(' '):
+                tuples.append((que, tag))
+        return tuples
 
     @staticmethod
     def convert_que_dict(que_dict: dict) -> (pd.DataFrame, pd.DataFrame):
@@ -150,14 +250,9 @@ class Predictor:
         """
         # get DataFrame from dict
         que_df = pd.DataFrame.from_dict(que_dict)
-        ques = que_df['questions_id'].values
 
         # create question-tag tuples
-        tuples = []
-        for i, tags in enumerate(que_df['questions_tags'].values):
-            que = ques[i]
-            for tag in tags.split(' '):
-                tuples.append((que, tag))
+        tuples = Formatter.__convert_tuples(que_df['questions_id'].values, que_df['questions_tags'].values)
 
         # create DataFrame from tuples
         que_tags = pd.DataFrame(tuples, columns=['tag_questions_question_id', 'tags_tag_name'])
@@ -176,11 +271,8 @@ class Predictor:
         pros = pro_df['professionals_id'].values
 
         # create professional-tag tuples
-        tuples = []
-        for i, tags in enumerate(pro_df['professionals_subscribed_tags'].values):
-            pro = pros[i]
-            for tag in tags.split(' '):
-                tuples.append((pro, tag))
+        tuples = Formatter.__convert_tuples(pro_df['professionals_id'].values,
+                                            pro_df['professionals_subscribed_tags'].values)
 
         # create DataFrame from tuples
         pro_tags = pd.DataFrame(tuples, columns=['tag_users_user_id', 'tags_tag_name'])
